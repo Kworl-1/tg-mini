16f6b9403e323514568e88b02fe3cb227bcecd51
Kworl-1 <57934578+Kworl-1@users.noreply.github.com>
2024-11-10 11:19:00 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+    <script async src="https://core.telegram.org/js/telegram-web-app.js"></script>
     <React.StrictMode>
         <App />
     </React.StrictMode>
