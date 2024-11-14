f9f0bddfc6da352bb9bd9db579409bd8f0542924
Kworl-1 <57934578+Kworl-1@users.noreply.github.com>
2024-11-14 16:25:48 +0300

    Update Leaderboard.js

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -9,7 +9,14 @@ function Leaderboard() {
         // Функция для получения данных с leaderboard
         const fetchLeaderboard = async () => {
             try {
-                const response = await axios.get('http://burro-distinct-implicitly.ngrok-free.app/api/leaderboard');
+                const response = await axios.get(
+                    'https://burro-distinct-implicitly.ngrok-free.app/api/leaderboard',
+                    {
+                        headers: {
+                            'ngrok-skip-browser-warning': 'true'  // Добавляем заголовок
+                        }
+                    }
+                );
                 setLeaderboard(response.data); // Сохраняем данные в состояние
             } catch (error) {
                 console.error('Ошибка при получении leaderboard:', error);
